ea441bd6aa5b8b2bed0f15bd1fe48d376a72487a
Adam Vinch <adam.vinch@federatedmail.com>
2025-10-22 16:30:41 -0400

    Temporarily point at GV review branch

--- web-sdks/dart/lib/src/client.dart
+++ web-sdks/dart/lib/src/client.dart
@@ -88,9 +88,11 @@ class GroupVanClientConfig {
     bool enableCaching = true,
   }) {
     return GroupVanClientConfig(
-      baseUrl: 'https://api.staging.groupvan.com',
+      baseUrl: 'https://catalog-sdk-integration.dev.groupvan.com',
+      //baseUrl: 'https://api.staging.groupvan.com',
       httpClientConfig: const HttpClientConfig(
-        baseUrl: 'https://api.staging.groupvan.com',
+        baseUrl: 'https://catalog-sdk-integration.dev.groupvan.com',
+        //baseUrl: 'https://api.staging.groupvan.com',
         enableLogging: true,
       ),
       tokenStorage: tokenStorage ?? SecureTokenStorage.platformOptimized(),
