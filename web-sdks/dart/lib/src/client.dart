f44fb26b150d19a4fe98419da5bbb5b941136f70
Adam Vinch <adam.vinch@federatedmail.com>
2025-11-17 11:10:54 -0500

    Point SDK back at original omni search branch

--- web-sdks/dart/lib/src/client.dart
+++ web-sdks/dart/lib/src/client.dart
@@ -89,10 +89,10 @@ class GroupVanClientConfig {
   }) {
     return GroupVanClientConfig(
       //baseUrl: 'https://api.staging.groupvan.com',
-      baseUrl: 'v3-search-copy',
+      baseUrl: 'https://381-v3-search-queries.dev.groupvan.com',
       httpClientConfig: const HttpClientConfig(
         //baseUrl: 'https://api.staging.groupvan.com',
-        baseUrl: 'v3-search-copy',
+        baseUrl: 'https://381-v3-search-queries.dev.groupvan.com',
         enableLogging: true,
       ),
       tokenStorage: tokenStorage ?? SecureTokenStorage.platformOptimized(),
