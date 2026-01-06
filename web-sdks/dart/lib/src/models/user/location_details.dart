6af56ea31e9dbb378b336430ad0319d20c25b792
adam <adam.vinch@federatedmail.com>
2026-01-06 13:03:14 -0500

    Add additional fields to location details model to match groupvan API

--- web-sdks/dart/lib/src/models/user/location_details.dart
+++ web-sdks/dart/lib/src/models/user/location_details.dart
@@ -2,12 +2,23 @@ class LocationDetails {
   final String? description;
   final double? latitude;
   final double? longitude;
+  final String? phone;
+  final String? address;
+  final String? city;
+  final String? state;
+  final String? zip;
   final Map<String, String?>? hours;
+
 
   LocationDetails({
     this.description,
     this.latitude,
     this.longitude,
+    this.phone,
+    this.address,
+    this.city,
+    this.state,
+    this.zip,
     this.hours,
   });
 
@@ -16,6 +27,11 @@ class LocationDetails {
       description: json['description'] as String?,
       latitude: (json['latitude'] as num?)?.toDouble(),
       longitude: (json['longitude'] as num?)?.toDouble(),
+      phone: json['phone'] as String?,
+      address: json['address'] as String?,
+      city: json['city'] as String?,
+      state: json['state'] as String?,
+      zip: json['zip'] as String?,
       hours: (json['hours'] as Map<String, dynamic>?)?.cast<String, String?>(),
     );
   }
@@ -26,6 +42,11 @@ class LocationDetails {
       'latitude': latitude,
       'longitude': longitude,
       'hours': hours,
+      'phone': phone,
+      'address': address,
+      'city': city,
+      'state': state,
+      'zip': zip,
     };
   }
 }
