52613a8dadcd5b7aa58f52c6332e4f9ba2710290
adam <adam.vinch@federatedmail.com>
2026-04-08 11:10:57 -0400

    Add highlight fields to model

--- web-sdks/dart/lib/src/models/search/vehicle_and_part_type.dart
+++ web-sdks/dart/lib/src/models/search/vehicle_and_part_type.dart
@@ -4,16 +4,31 @@ import '../shared.dart';
 class VehicleAndPartType {
   final Vehicle vehicle;
   final PartType partType;
+  final List<Map<String, dynamic>> vehicleHighlights;
+  final List<Map<String, dynamic>> partTypeHighlights;
 
-  VehicleAndPartType({required this.vehicle, required this.partType});
+  VehicleAndPartType({
+    required this.vehicle,
+    required this.partType,
+    this.vehicleHighlights = const [],
+    this.partTypeHighlights = const [],
+  });
 
   factory VehicleAndPartType.fromJson(Map<String, dynamic> json) => VehicleAndPartType(
     vehicle: Vehicle.fromJson(json['vehicle']),
     partType: PartType.fromJson(json['part_type']),
+    vehicleHighlights: (json['vehicle_highlights'] as List<dynamic>?)
+        ?.map((e) => Map<String, dynamic>.from(e as Map))
+        .toList() ?? [],
+    partTypeHighlights: (json['part_type_highlights'] as List<dynamic>?)
+        ?.map((e) => Map<String, dynamic>.from(e as Map))
+        .toList() ?? [],
   );
 
   Map<String, dynamic> toJson() => {
     'vehicle': vehicle.toJson(),
     'part_type': partType.toJson(),
+    'vehicle_highlights': vehicleHighlights,
+    'part_type_highlights': partTypeHighlights,
   };
 }
